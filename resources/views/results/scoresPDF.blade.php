0dd5798a550e9968c9c5df5da1f3c856aef6f8ef
Darragh Sherwin <dsherwin@overstock.com>
2018-05-21 14:40:45 +0100

    Bigger image on PDF

--- resources/views/results/scoresPDF.blade.php
+++ resources/views/results/scoresPDF.blade.php
@@ -2,179 +2,153 @@
 
 @section('content')
 
-
-    <div class="container">
-
-        <div class="panel panel-default">
-            <div class="panel-heading"><h2>Individual Scores for {{ $clubname }}</h2></div>
-
-            <div class="panel-body">
-                @if( $colour )
-                    <div class="row">
-                        <div class="col-xs-12">
-                            <h3>Colour Images</h3>
-                        </div>
-
-                    </div>
-                    <hr/>
-                    <div class="row top-buffer">
-                        <div class="col-xs-6">
-                            <div class="row">
-                                <div class="col-xs-3">
-                                    <h4>Author</h4>
-                                </div>
-                                <div class="col-xs-3">
-                                    <h4>Title</h4>
-                                </div>
-                                <div class="col-xs-3">
-                                    <h4>Judge 1</h4>
-                                </div>
-                                <div class="col-xs-3">
-                                    <h4>Judge 2</h4>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-xs-6">
-                            <div class="row">
-                                <div class="col-xs-3">
-                                    <h4>Judge 3</h4>
-                                </div>
-                                <div class="col-xs-3">
-                                    <h4>Total</h4>
-                                </div>
-                                <div class="col-xs-3">
-                                    <h4>Award</h4>
-                                </div>
-                                <div class="col-xs-3">
-                                    <h4>Image</h4>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    @foreach($colour as $result)
-                        <div class="row top-buffer">
-                            <div class="col-xs-6">
-                                <div class="row">
-                                    <div class="col-xs-3">
-                                        {{$result['author']}}
-                                    </div>
-                                    <div class="col-xs-3">
-                                        {{$result['title']}}
-                                    </div>
-                                    <div class="col-xs-3">
-                                        {{$result['judge1']}}
-                                    </div>
-                                    <div class="col-xs-3">
-                                        {{$result['judge2']}}
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-xs-6">
-                                <div class="row">
-                                    <div class="col-xs-3">
-                                        {{$result['judge3']}}
-                                    </div>
-                                    <div class="col-xs-3">
-                                        {{$result['total']}}
-                                    </div>
-                                    <div class="col-xs-3">
-                                        {{$result['award']}}
-                                    </div>
-                                    <div class="col-xs-3">
-                                        <img src="{{ url('/') }}/uploads/200_{{$result['image']}}" width="100px">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <hr/>
-                    @endforeach
-                @endif
+    <div style="padding:  15px; margin: 15px;">
+        <div class="row" style="text-align: center">
+            <img src="http://irishphoto.ie/wp-content/uploads/2015/08/cropped-ipf-banner4.png">
+        </div>
+        <div class="row" style="text-align: center">
+            <h1>Individual Scores for {{ $clubname }}</h1>
+        </div>
 
 
-                @if( $mono )
-                    <div class="row">
-                        <div class="col-xs-12">
-                            <h3>Monochrome Images</h3>
-                        </div>
-
-                    </div>
-
-                    <hr/>
-                    <div class="row top-buffer">
-                        <div class="col-xs-6">
-                            <div class="row">
-                                <div class="col-xs-3">
-                                    <h4>Author</h4>
-                                </div>
-                                <div class="col-xs-3">
-                                    <h4>Title</h4>
-                                </div>
-                                <div class="col-xs-3">
-                                    <h4>Judge 1</h4>
-                                </div>
-                                <div class="col-xs-3">
-                                    <h4>Judge 2</h4>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-xs-6">
-                            <div class="row">
-                                <div class="col-xs-3">
-                                    <h4>Judge 3</h4>
-                                </div>
-                                <div class="col-xs-3">
-                                    <h4>Total</h4>
-                                </div>
-                                <div class="col-xs-3">
-                                    <h4>Award</h4>
-                                </div>
-                                <div class="col-xs-3">
-                                    <h4>Image</h4>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    @foreach($mono as $result)
-                        <div class="row top-buffer">
-                            <div class="col-xs-6">
-                                <div class="row">
-                                    <div class="col-xs-3">
-                                        {{$result['author']}}
-                                    </div>
-                                    <div class="col-xs-3">
-                                        {{$result['title']}}
-                                    </div>
-                                    <div class="col-xs-3">
-                                        {{$result['judge1']}}
-                                    </div>
-                                    <div class="col-xs-3">
-                                        {{$result['judge2']}}
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-xs-6">
-                                <div class="row">
-                                    <div class="col-xs-3">
-                                        {{$result['judge3']}}
-                                    </div>
-                                    <div class="col-xs-3">
-                                        {{$result['total']}}
-                                    </div>
-                                    <div class="col-xs-3">
-                                        {{$result['award']}}
-                                    </div>
-                                    <div class="col-xs-3">
-                                        <img src="{{ url('/') }}/uploads/200_{{$result['image']}}" width="100px">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <hr/>
-                    @endforeach
-                @endif
+        <hr/>
+        @if( $colour )
+            <div class="row">
+                <h3>Colour Images</h3>
 
             </div>
-        </div>
+
+            <table style="border-collapse: unset; border-spacing: 10px; width: 100%;">
+                <tr>
+                    <th class="col-xs-3">
+                        <h4>Author</h4>
+                    </th>
+                    <th class="col-xs-3">
+                        <h4>Title</h4>
+                    </th>
+                    <th class="col-xs-3">
+                        <h4>Judge 1</h4>
+                    </th>
+                    <th class="col-xs-3">
+                        <h4>Judge 2</h4>
+                    </th>
+
+                    <th class="col-xs-3">
+                        <h4>Judge 3</h4>
+                    </th>
+                    <th class="col-xs-3">
+                        <h4>Total</h4>
+                    </th>
+                    <th class="col-xs-3">
+                        <h4>Award</h4>
+                    </th>
+                    <th class="col-xs-3">
+                        <h4>Image</h4>
+                    </th>
+                </tr>
+                @foreach($colour as $result)
+                    <tr>
+
+                        <td class="col-xs-3">
+                            {{$result['author']}}
+                        </td>
+                        <td class="col-xs-3">
+                            {{$result['title']}}
+                        </td>
+                        <td class="col-xs-3">
+                            {{$result['judge1']}}
+                        </td>
+                        <td class="col-xs-3">
+                            {{$result['judge2']}}
+                        </td>
+                        <td class="col-xs-3">
+                            {{$result['judge3']}}
+                        </td>
+                        <td class="col-xs-3">
+                            {{$result['total']}}
+                        </td>
+                        <td class="col-xs-3">
+                            {{$result['award']}}
+                        </td>
+                        <td class="col-xs-3">
+                            <img src="{{ url('/') }}/uploads/200_{{$result['image']}}" width="100px">
+                        </td>
+                    </tr>
+                @endforeach
+            </table>
+
+            <hr/>
+
+        @endif
+
+
+        @if( $mono )
+            <div class="row">
+
+                <h3>Monochrome Images</h3>
+
+            </div>
+
+            <table style="border-collapse: unset; border-spacing: 10px; width: 100%;">
+                <tr>
+                    <th class="col-xs-3">
+                        <h4>Author</h4>
+                    </th>
+                    <th class="col-xs-3">
+                        <h4>Title</h4>
+                    </th>
+                    <th class="col-xs-3">
+                        <h4>Judge 1</h4>
+                    </th>
+                    <th class="col-xs-3">
+                        <h4>Judge 2</h4>
+                    </th>
+
+                    <th class="col-xs-3">
+                        <h4>Judge 3</h4>
+                    </th>
+                    <th class="col-xs-3">
+                        <h4>Total</h4>
+                    </th>
+                    <th class="col-xs-3">
+                        <h4>Award</h4>
+                    </th>
+                    <th class="col-xs-3">
+                        <h4>Image</h4>
+                    </th>
+                </tr>
+                @foreach($mono as $result)
+                    <tr>
+
+                        <td class="col-xs-3">
+                            {{$result['author']}}
+                        </td>
+                        <td class="col-xs-3">
+                            {{$result['title']}}
+                        </td>
+                        <td class="col-xs-3">
+                            {{$result['judge1']}}
+                        </td>
+                        <td class="col-xs-3">
+                            {{$result['judge2']}}
+                        </td>
+                        <td class="col-xs-3">
+                            {{$result['judge3']}}
+                        </td>
+                        <td class="col-xs-3">
+                            {{$result['total']}}
+                        </td>
+                        <td class="col-xs-3">
+                            {{$result['award']}}
+                        </td>
+                        <td class="col-xs-3">
+                            <img src="{{ url('/') }}/uploads/200_{{$result['image']}}" width="100px">
+                        </td>
+                    </tr>
+                @endforeach
+            </table>
+
+        @endif
     </div>
 
 @endsection
--- resources/views/results/standingsPDF.blade.php
+++ resources/views/results/standingsPDF.blade.php
@@ -2,108 +2,108 @@
 
 @section('content')
 
-    <div class="row" style="text-align: center">
-        <img src="http://irishphoto.ie/wp-content/uploads/2015/08/cropped-ipf-banner4.png">
-    </div>
-    <div class="row" style="text-align: center">
-        <h2>IPF Club Championship 2018</h2>
-    </div>
+    <div style="padding:  15px; margin: 15px;">
+        <div class="row" style="text-align: center">
+            <img src="http://irishphoto.ie/wp-content/uploads/2015/08/cropped-ipf-banner4.png">
+        </div>
+        <div class="row" style="text-align: center">
+            <h1>IPF Club Championship 2018 - Standings</h1>
+        </div>
 
 
+        <div class="row">
+            <h2>Overall Standings</h2>
 
-    <div class="row">
-        <div class="col-xs-12"><h2>Overall Standings</h2></div>
+        </div>
+        <div class="row">
 
-    </div>
-    <div class="row">
-
-        <table style="border-collapse: unset; border-spacing: 10px; width: 100%">
-            <tr>
-                <th><h4>Clubname</h4></th>
-                <th><h3>Score</h3></th>
-                <th><h3>Place</h3></th>
-                <th><h3>Panels</h3></th>
-            </tr>
-            @foreach ($overall as $result)
+            <table style="border-collapse: unset; border-spacing: 10px; width: 100%;">
                 <tr>
-
-                    <td><h4>{{ $result['clubname'] }}</h4></td>
-                    <td>{{ $result['score'] }}</td>
-                    <td>{{ $result['place'] }}</td>
-                    <td><img
-                                src="{{ url('/') }}/uploads/{{$result['club_id']}}/{{$result['club_id']}}_mono_contact_sheet.jpg"
-                                width="200px"><br/><br/><img
-                                src="{{ url('/') }}/uploads/{{$result['club_id']}}/{{$result['club_id']}}_colour_contact_sheet.jpg"
-                                width="200px"></td>
+                    <th><h4>Clubname</h4></th>
+                    <th><h3>Score</h3></th>
+                    <th><h3>Place</h3></th>
+                    <th><h3>Panels</h3></th>
                 </tr>
-            @endforeach
-        </table>
-    </div>
+                @foreach ($overall as $result)
+                    <tr style="border-collapse: unset; border-spacing: 10px;border-top: black 2px solid">
 
-    <hr/>
+                        <td><h4>{{ $result['clubname'] }}</h4></td>
+                        <td>{{ $result['score'] }}</td>
+                        <td>{{ $result['place'] }}</td>
+                        <td><img
+                                    src="{{ url('/') }}/uploads/{{$result['club_id']}}/{{$result['club_id']}}_mono_contact_sheet.jpg"
+                                    width="200px"><br/><br/><img
+                                    src="{{ url('/') }}/uploads/{{$result['club_id']}}/{{$result['club_id']}}_colour_contact_sheet.jpg"
+                                    width="200px"></td>
+                    </tr>
+                @endforeach
+            </table>
+        </div>
 
-    <div class="row">
-        <div class="col-xs-12"><h2>Colour Standings</h2></div>
+        <hr/>
 
-    </div>
+        <div class="row">
+            <h2>Colour Standings</h2>
 
-    <div class="row">
+        </div>
 
-        <table style="border-collapse: unset; border-spacing: 10px; width: 100%">
-            <tr>
-                <th><h4>Clubname</h4></th>
-                <th><h3>Score</h3></th>
-                <th><h3>Place</h3></th>
-                <th><h3>Panels</h3></th>
-            </tr>
+        <div class="row">
 
-            @foreach ($colour as $result)
-                <tr style="padding-bottom: 20px">
-
-                    <td><h4>{{ $result['clubname'] }}</h4></td>
-                    <td>{{ $result['score'] }}</td>
-                    <td>{{ $result['place'] }}</td>
-                    <td><img
-                                src="{{ url('/') }}/uploads/{{$result['club_id']}}/{{$result['club_id']}}_colour_contact_sheet.jpg"
-                                width="200px"></td>
-                </tr>
-            @endforeach
-        </table>
-    </div>
-    <hr/>
-
-
-    <div class="row">
-        <div class="col-xs-12"><h2>Monochrome Standings</h2></div>
-
-    </div>
-
-    <div class="row">
-
-        <table style="border-collapse: unset; border-spacing: 10px; width: 100%">
-            <tr>
-                <th><h4>Clubname</h4></th>
-                <th><h3>Score</h3></th>
-                <th><h3>Place</h3></th>
-                <th><h3>Panels</h3></th>
-            </tr>
-
-            @foreach ($mono as $result)
+            <table style="border-collapse: unset; border-spacing: 10px; width: 100%">
                 <tr>
-
-                    <td><h4>{{ $result['clubname'] }}</h4></td>
-                    <td>{{ $result['score'] }}</td>
-                    <td>{{ $result['place'] }}</td>
-                    <td><img
-                                src="{{ url('/') }}/uploads/{{$result['club_id']}}/{{$result['club_id']}}_mono_contact_sheet.jpg"
-                                width="200px"></td>
+                    <th><h4>Clubname</h4></th>
+                    <th><h3>Score</h3></th>
+                    <th><h3>Place</h3></th>
+                    <th><h3>Panels</h3></th>
                 </tr>
-        @endforeach
 
+                @foreach ($colour as $result)
+                    <tr style="padding-bottom: 20px">
+
+                        <td><h4>{{ $result['clubname'] }}</h4></td>
+                        <td>{{ $result['score'] }}</td>
+                        <td>{{ $result['place'] }}</td>
+                        <td><img
+                                    src="{{ url('/') }}/uploads/{{$result['club_id']}}/{{$result['club_id']}}_colour_contact_sheet.jpg"
+                                    width="200px"></td>
+                    </tr>
+                @endforeach
+            </table>
+        </div>
+        <hr/>
+
+
+        <div class="row">
+            <h2>Monochrome Standings</h2>
+
+        </div>
+
+        <div class="row">
+
+            <table style="border-collapse: unset; border-spacing: 10px; width: 100%">
+                <tr>
+                    <th><h4>Clubname</h4></th>
+                    <th><h3>Score</h3></th>
+                    <th><h3>Place</h3></th>
+                    <th><h3>Panels</h3></th>
+                </tr>
+
+                @foreach ($mono as $result)
+                    <tr>
+
+                        <td><h4>{{ $result['clubname'] }}</h4></td>
+                        <td>{{ $result['score'] }}</td>
+                        <td>{{ $result['place'] }}</td>
+                        <td><img
+                                    src="{{ url('/') }}/uploads/{{$result['club_id']}}/{{$result['club_id']}}_mono_contact_sheet.jpg"
+                                    width="200px"></td>
+                    </tr>
+            @endforeach
+
+
+        </div>
 
     </div>
-
-
 
 
 @endsection
