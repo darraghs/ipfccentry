01b899d8832313138b959f1dff37f5ff84208a27
Darragh Sherwin <dsherwin@overstock.com>
2018-05-21 12:43:40 +0100

    Bigger image on PDF

--- resources/views/results/standingsPDF.blade.php
+++ resources/views/results/standingsPDF.blade.php
@@ -17,20 +17,20 @@
     </div>
     <div class="row">
 
-        <table style="width:  100%">
+        <table style="border-collapse: unset; border-spacing: 10px; width: 100%">
             <tr>
-                <th style="width: 25%"><h4>Clubname</h4></th>
-                <th style="width: 10%"><h3>Score</h3></th>
-                <th style="width: 10%"><h3>Place</h3></th>
-                <th style="width: 55%"><h3>Panels</h3></th>
+                <th><h4>Clubname</h4></th>
+                <th><h3>Score</h3></th>
+                <th><h3>Place</h3></th>
+                <th><h3>Panels</h3></th>
             </tr>
             @foreach ($overall as $result)
                 <tr>
 
-                    <td style="width: 25%"><h4>{{ $result['clubname'] }}</h4></td>
-                    <td style="width: 10%">{{ $result['score'] }}</td>
-                    <td style="width: 10%">{{ $result['place'] }}</td>
-                    <td style="width: 55%"><img
+                    <td><h4>{{ $result['clubname'] }}</h4></td>
+                    <td>{{ $result['score'] }}</td>
+                    <td>{{ $result['place'] }}</td>
+                    <td><img
                                 src="{{ url('/') }}/uploads/{{$result['club_id']}}/{{$result['club_id']}}_mono_contact_sheet.jpg"
                                 width="200px"><br/><br/><img
                                 src="{{ url('/') }}/uploads/{{$result['club_id']}}/{{$result['club_id']}}_colour_contact_sheet.jpg"
@@ -49,7 +49,7 @@
 
     <div class="row">
 
-        <table>
+        <table style="border-collapse: unset; border-spacing: 10px; width: 100%">
             <tr>
                 <th><h4>Clubname</h4></th>
                 <th><h3>Score</h3></th>
@@ -58,7 +58,7 @@
             </tr>
 
             @foreach ($colour as $result)
-                <tr>
+                <tr style="padding-bottom: 20px">
 
                     <td><h4>{{ $result['clubname'] }}</h4></td>
                     <td>{{ $result['score'] }}</td>
@@ -80,7 +80,7 @@
 
     <div class="row">
 
-        <table>
+        <table style="border-collapse: unset; border-spacing: 10px; width: 100%">
             <tr>
                 <th><h4>Clubname</h4></th>
                 <th><h3>Score</h3></th>
